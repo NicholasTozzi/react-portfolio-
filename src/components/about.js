609c83a21fbb273a64b41fc6f446f7e893d68bde
Tozzi <nicholastozzi@gmail.com>
2023-04-08 04:09:56 -0400

    worked on adding base bootstrap components. Still need to add contact modal and footer

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,36 @@
-import React from "react";
-//TODO: Edit Footer Template
-export default function About() {
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Tab from "react-bootstrap/Tab";
+
+function About() {
   return (
-    <div class="container">
-      <h1>About me!</h1>
-    </div>
+    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+      <Row>
+        <Col sm={3}>
+          <Nav variant="pills" className="flex-column">
+            <Nav.Item>
+              <Nav.Link eventKey="first">Tab 1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">Tab 2</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col sm={9}>
+          <Tab.Content>
+            <Tab.Pane eventKey="first">
+              <h1>wahoo</h1>
+            </Tab.Pane>
+
+            <Tab.Pane eventKey="second">
+              <h1>yeehaw</h1>
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
   );
 }
+
+export default About;
--- src/components/resume.js
+++ src/components/resume.js
@@ -6,43 +6,47 @@ function Resume() {
   return (
     <Carousel>
       <Carousel.Item>
-        <a href="https://github.com/NicholasTozzi/Social-Network-API">
+        <a href="https://flick-tracker.herokuapp.com/">
           <img
             className="d-block w-100"
-            src="https://kinsta.com/wp-content/uploads/2022/05/what-is-mongodb.jpg"
-            alt="Social Media API"
+            src="https://www.gannett-cdn.com/-mm-/eb34b9888702cb3f4641b6ee49362a859a5dfd45/c=0-0-2309-1299/local/-/media/2020/04/29/USATODAY/usatsports/movie-theater-gettyimages-530640869.jpg?width=2309&height=1299&fit=crop&format=pjpg&auto=webp"
+            alt="Flick Tracker"
           />
         </a>
         <Carousel.Caption>
-          <h3>Social Media API</h3>
-          <img src="/public/github.png" /> //!Image not currently linking
-          <p>A backend Social Media API created with mongoDB.</p>
+          <h3>Flick Tracker</h3>
+          <img src="/public/github.png" />
+          <p>A MERN Movie App to track watched movies and your watchlist</p>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://kinsta.com/wp-content/uploads/2022/05/what-is-mongodb.jpg"
-          alt="Social Media API"
-        />
+        <a href="https://github.com/NicholasTozzi/e-commerce">
+          <img
+            className="d-block w-100"
+            src="https://e0.pxfuel.com/wallpapers/850/829/desktop-wallpaper-e-commerce-web-designing-development-company-e-commerce.jpg"
+            alt="e-commerce"
+          />
+        </a>
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>E-Commerce BackEnd Application</h3>
+          <p>A mySQL backend appplication for an ecommerce website.</p>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://kinsta.com/wp-content/uploads/2022/05/what-is-mongodb.jpg"
-          alt="Social Media API"
-        />
+        <a href="https://alexbrenes999.github.io/project1/">
+          <img
+            className="d-block w-100"
+            src="https://kinsta.com/wp-content/uploads/2022/05/what-is-mongodb.jpg"
+            alt="Game Wiki"
+          />
+        </a>
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h3>Game Wiki</h3>
+          <p>An application to find video games based on catagory.</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
