5362f6bc458085191b932061c1eaaead34028753
Tozzi <nicholastozzi@gmail.com>
2023-04-11 02:44:48 -0400

    .

--- src/components/about.js
+++ src/components/about.js
@@ -1,26 +1,60 @@
+import "../styles/About.css";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Row from "react-bootstrap/Row";
 import Tab from "react-bootstrap/Tab";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import ProfilePicture from "../../src/pictures/me.jpg";
 
 function About() {
   return (
     <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-      <Row>
+      <Row className="row">
         <Col sm={3}>
-          <Nav variant="pills" className="flex-column">
-            <Nav.Item>
-              <Nav.Link eventKey="first">Tab 1</Nav.Link>
+          <Nav variant="pills" className="flex-column about-nav-container">
+            <Nav.Item className="about-pills">
+              <Nav.Link eventKey="first">About Me</Nav.Link>
             </Nav.Item>
+
             <Nav.Item>
-              <Nav.Link eventKey="second">Tab 2</Nav.Link>
+              <Nav.Link className="about-pills" eventKey="second">
+                Future
+              </Nav.Link>
             </Nav.Item>
           </Nav>
         </Col>
         <Col sm={9}>
           <Tab.Content>
-            <Tab.Pane eventKey="first">
-              <h1>wahoo</h1>
+            <Tab.Pane className="wahoo" eventKey="first">
+              <h1
+                className=" about-pane-right
+          "
+              >
+                wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo
+                wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo
+                wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo
+                wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo
+                wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo wahoo
+                wahoo wahoo wahoo wahoo
+              </h1>
+              <div className="social-container">
+                <a href="https://twitter.com/SeitanETH">
+                  <FaTwitter className="icon" />
+                </a>
+                <a href="https://twitter.com/SeitanETH">
+                  <FaLinkedin className="icon" />
+                </a>
+                <a href="https://twitter.com/SeitanETH">
+                  <FaGithub className="icon" />
+                </a>
+              </div>
+              <img
+                className="ProfilePicture"
+                alt="its me"
+                src={ProfilePicture}
+              />
             </Tab.Pane>
 
             <Tab.Pane eventKey="second">
